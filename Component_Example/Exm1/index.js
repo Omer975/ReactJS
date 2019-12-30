ace7d5dc7eb98ed25dba68911ec554136183a19a
Ömer <55637993+Omer975@users.noreply.github.com>
2019-12-30 22:04:23 +0300

    Update index.js

--- Component_Example/Exm1/index.js
+++ Component_Example/Exm1/index.js
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import User from "./Components/User.js";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Applications</h4>
+      <User />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
