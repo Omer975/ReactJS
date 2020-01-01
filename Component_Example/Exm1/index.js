dd04448b3dfeff18ea05bf30e74a78b39b730083
Ömer <55637993+Omer975@users.noreply.github.com>
2020-01-01 17:35:33 +0300

    Update index.js

--- Component_Example/Exm1/index.js
+++ Component_Example/Exm1/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import User from "./Components/User.js";
+import Navbar from "./Components/navbar.js";
+import Prop from "./Components/props";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h4>Applications</h4>
+      {/* <h4>Applications</h4> */}
+      <Navbar />
+      <hr />
       <User />
+      <Prop Name="Ömer" Department="IT" Company="Malmo University" />
+      <Prop
+        Name="Zeynep"
+        Department="Child Development"
+        Company="Lund University"
+      />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
